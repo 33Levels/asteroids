82e3ba0559bdd424a52eb10bb2b52a5921c8ab08
33Levels <tobeinfire@gmail.com>
2026-01-07 13:35:48 +0100

    Add asteroids splitting

--- player.py
+++ player.py
@@ -1,6 +1,14 @@
 import pygame
+import sys
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from constants import (
+        LINE_WIDTH, 
+        PLAYER_RADIUS, 
+        PLAYER_TURN_SPEED, 
+        PLAYER_SPEED, 
+        PLAYER_SHOOT_SPEED, 
+        PLAYER_SHOOT_COOLDOWN_SECONDS
+    )
 from shot import Shot
 
 class Player(CircleShape):
@@ -8,6 +16,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
@@ -24,6 +33,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
     
     def update(self, dt):
+        self.shot_cooldown -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -36,6 +46,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        if keys[pygame.K_ESCAPE]:
+            sys.exit()
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
@@ -44,5 +56,8 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
     
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        if self.shot_cooldown > 0:
+            return
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+        shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
